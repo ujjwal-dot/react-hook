4856c24b676250bb7220930ce282bc0dbc4d0693
Ujjwal Kumar Das <ujjwaldas1059@gmail.com>
2023-03-27 22:19:50 +0530

    useState with object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import HooksCounter from './components/HooksCounter';
+import HooksCounterTwo from './components/HooksCounterTwo';
 
 function App() {
   return (
     <div className="App">
-     <HooksCounter/>
+     <HooksCounterTwo/>
     </div>
   );
 }
